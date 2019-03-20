6aae6c73ebe30d14f0ce6c58d972a7d62e44b42f
Joshua Vance <skinsfandc0612@icloud.com>
2019-03-20 18:47:46 -0400

    password hash and salt

--- models/user.js
+++ models/user.js
@@ -1,8 +1,34 @@
-const mongoose = require("../db/connection");
+const mongoose = require('../db/connection');
+var uniqueValidator = require('mongoose-unique-validator');
+var bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({
-    email: String,
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, 'is invalid'],
+      index: true,
+    },
     password: String,
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+//configuring the validator's message to say that the field "is already taken."
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
 module.exports = UserSchema;
